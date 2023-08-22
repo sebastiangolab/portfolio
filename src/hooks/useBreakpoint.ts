40bb9a2c9e2b740a7aa397a492e80bc8ff128a44
sebastiangolab <sebagolab97@gmail.com>
2023-08-22 22:42:10 +0200

    create useBreakpoint

---
+++ src/hooks/useBreakpoint.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+export enum Breakpoints {
+   START = 0,
+   MOBILE = 767,
+   TABLET = 991,
+   SMALL_DESKTOP = 1399,
+}
+
+const useBreakpoint = (min: Breakpoints, max?: Breakpoints) => {
+   const [isBreakpoint, setIsBreakpoint] = useState<boolean>(checkBreakpoint());
+   console.log(isBreakpoint);
+
+   function checkBreakpoint(): boolean {
+      const screenWidth = window.innerWidth;
+
+      if (max !== undefined) {
+         return screenWidth > min && screenWidth <= max;
+      } else {
+         return screenWidth > min;
+      }
+   }
+
+   function handleResize(): void {
+      setIsBreakpoint(checkBreakpoint());
+   }
+
+   useEffect(() => {
+      window.addEventListener('resize', handleResize);
+      return () => {
+         window.removeEventListener('resize', handleResize);
+      };
+   }, []);
+
+   return isBreakpoint;
+};
+
+export default useBreakpoint;
